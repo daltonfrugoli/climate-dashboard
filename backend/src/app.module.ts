220d6b58e71181edbfa0ddc1909fd8458852bd46
Dalton Frugoli <daltonfrugoli7@gmail.com>
2025-12-01 17:27:47 -0300

    feat(backend): integrate Weather module into application

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -5,6 +5,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
+import { WeatherModule } from './weather/weather.module';
 
 @Module({
   imports: [
@@ -20,7 +21,7 @@ import { UsersModule } from './users/users.module';
     // Módulos da aplicação
     AuthModule,
     UsersModule,
-    // WeatherModule,
+    WeatherModule,
     // ExternalApiModule,
   ],
   controllers: [AppController],
